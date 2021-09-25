0d86546554f3d70b9579feb93b5e9c78f9694dfc
JAMart <jorge.martinez1597@gmail.com>
2021-09-25 18:05:39 -0400

    created User, and Thought models along with a reactionSchema in Thought.js

---
+++ models/Thought.js
@@ -0,0 +1,72 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String, 
+            required: 'Please add something to your reaction!',
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: 'Every reaction needs a user to create it! So who created this one?'
+        },
+        createdAt: {
+            Type: Date,
+            default: Date.now,
+            get: (createdVal) => dateFormat(createdVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);s
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: 'You can\'t have an empty Thought!',
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdVal) => dateFormat(createdVal)
+        },
+        username: {
+            type: String,
+            required: 'Every thought needs a user to create it! So who created this one?'
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true, 
+            getters: true
+        },
+        id: false
+    }
+)
+
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length + 1;
+});
+
+
+const Thought = model('Thought', ThoughtSchema);
+
+
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -14,7 +14,37 @@ const UserSchema = new Schema(
             type: String,
             unique: true,
             required: 'You need to provide an email in order to create a User!',
-            
+            // I found this regex on Stack Overflow from a user named ramon22
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
         }
     }
-)
+);
+
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length + 1;
+});
+
+
+const User = model('User', UserSchema);
+
+
+
+module.exports = User;
